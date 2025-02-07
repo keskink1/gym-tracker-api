88114b10a01902f41ba99877f2ad1119cee23051
Kaan Keskin <kaankeskin1@hotmail.com>
2025-02-07 19:03:53 +0300

    timers

--- models/workouts.js
+++ models/workouts.js
@@ -1,43 +1,64 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const workoutSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 3,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  exercises: [
-    {
-      exerciseId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Exercise",
-        required: true,
-      },
-      sets: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      reps: {
-        type: Number,
-        required: true,
-        min: 1,
-      },
-      weight: {
-        type: Number,
-        min: 0,
-        required: false,
-      },
-      notes: String,
+const workoutSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
     },
-  ],
-});
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    exercises: [
+      {
+        exerciseId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Exercise",
+          required: true,
+        },
+        sets: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        reps: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        weight: {
+          type: Number,
+          min: 0,
+          required: false,
+        },
+        sessions: [
+          {
+            exerciseTime: {
+              type: Number,
+              required: true,
+              min: 0,
+            },
+            restTime: {
+              type: Number,
+              required: true,
+              min: 0,
+            },
+            completedAt: {
+              type: Date,
+              required: true,
+            },
+          },
+        ],
+        notes: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
@@ -51,6 +72,15 @@ function validateWorkout(workout) {
           sets: Joi.number().min(1).required(),
           reps: Joi.number().min(1).required(),
           weight: Joi.number().min(0).optional(),
+          sessions: Joi.array()
+            .items(
+              Joi.object({
+                exerciseTime: Joi.number().min(0).required(),
+                restTime: Joi.number().min(0).required(),
+                completedAt: Joi.date().required(),
+              })
+            )
+            .optional(),
           notes: Joi.string().optional(),
         })
       )
